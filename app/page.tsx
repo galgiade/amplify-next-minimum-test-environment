33f4bf40fc9b784d3f71e6041dce54831bf640d0
matsui kazuaki <connectechceomatsui@gmail.com>
2025-09-27 14:49:13 +0900

    secretを使用するのは諦めて、ビルド時にシークレットキーを生成するように編集

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,13 @@
-import { secret } from "@aws-amplify/backend";
 import { connection } from "next/server";
 
 export default async function App() {
   await connection()
-  
-  // ローカル環境変数
-  const localStripeKey = process.env.STRIPE_SECRET_KEY;
-  
-  // Amplifyシークレット
-  let amplifySecret = 'Not available';
-  try {
-    const secretValue = await secret('STRIPE_SECRET_KEY');
-    amplifySecret = secretValue.toString();
-  } catch (error) {
-    console.error('Error getting secret:', error);
-  }
+  const stripeKey = process.env.STRIPE_SECRET_KEY;
   
   return (
     <div>
-      <h1>Local Stripe Secret Key</h1>
-      <h1>{localStripeKey || 'Not set'}</h1>
-      <h1>Amplify Secret Key</h1>
-      <h1>{amplifySecret}</h1>
+      <h1>hosting Stripe Secret Key</h1>
+      <h1>{stripeKey}</h1>
     </div>
   );
 }
