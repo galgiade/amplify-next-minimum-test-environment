b620b782305dc32c1679bdaf9b07c437e050394d
matsui kazuaki <connectechceomatsui@gmail.com>
2025-09-27 14:30:31 +0900

    secretを追加してみる

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+import { secret } from "@aws-amplify/backend";
 import { connection } from "next/server";
 
 export default async function App() {
-  await connection() // ランタイム環境変数を有効化
-  // 直接ランタイムで環境変数を取得
+  await connection()
+  
+  // ローカル環境変数
   const localStripeKey = process.env.STRIPE_SECRET_KEY;
-  console.log("localStripeKey", localStripeKey);
+  
+  // Amplifyシークレット
+  let amplifySecret = 'Not available';
+  try {
+    const secretValue = await secret('STRIPE_SECRET_KEY');
+    amplifySecret = secretValue.toString();
+  } catch (error) {
+    console.error('Error getting secret:', error);
+  }
+  
   return (
     <div>
       <h1>Local Stripe Secret Key</h1>
-        <h1>{localStripeKey || 'Not set'}</h1>
+      <h1>{localStripeKey || 'Not set'}</h1>
+      <h1>Amplify Secret Key</h1>
+      <h1>{amplifySecret}</h1>
     </div>
   );
 }
